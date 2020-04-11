a1492b931271d90ba6db432c94bb7dd68e159f89
ashrithchirutani <sairao.sr@gmail.com>
2020-04-11 19:31:39 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import './cssfile.css';
 
 /*
  * A simple React component
@@ -27,6 +28,44 @@ const myTreeData = [
     ],
   },
 ];
+
+const remainingNames = new Set(
+  [
+    "Vedha",
+    "Amit", 
+    "Akash",
+    "Ashrith",
+    "Lakshminarayanan",
+    "Piyush",
+    "Pranav",
+    "Ritesh",
+    "Saurabh Anand",
+    "Shafeeq",
+    "Upendra",
+    "Vijith",
+    "Abhishek Sahu",
+    "Abhishek Verma",
+    "Gnanadeep",
+    "Himanshu",
+    "Nikunj",
+    "Harshini",
+    "Sumant",
+    "Vaishal",
+    "Disha",
+    "Gautam",
+    "Ranganathan",
+    "Shambhavi",
+    "Shubham",
+    "Sindhura",
+    "Surendhar",
+    "Tushar",
+    "Yash",
+    "Yogesh","Anjali", "Ayush", "Chirag", "Hemanth", "GCS", "Naveen V",
+    "Navneet", "Nupur", "Saksham", "Sanchit"
+
+  ]
+);
+
 const map = {
   "Vedha" : ["Farhad", "Disha", "Vaishal", "Pranav"],
   "Vaishal": ["Shubham", "Ayush", "Himanshu"],
@@ -34,27 +73,34 @@ const map = {
   "Shubham": ["Tushar", "Yogesh", "Piyush"],
   "Piyush": ["Amit", "Abhishek Sahu", "Disha"],
   "Tushar":["Gautam", "Yash", "Anjali"],
-  "Farhad":["Surendhar", "Bhavik", "Unknown"],
+  "Farhad":["Surendhar", "Bhavik", "+91 90661 48817"],
   "Prashant":[],
-  "Ritesh":["Harshini", "Gnanadeep", "Narayana"],
-  "Abhishek Sahu":["Himanshu", "Nikunj", "Suman"],
+  "Ritesh":["Harshini", "Gnanadeep", "Lakshminarayanan"],
+  "Abhishek Sahu":["Himanshu", "Nikunj", "Sumant"],
   "Gnanadeep":["Naveen V", "Hemanth", "Ashrith"],
-  "Harshini":["Abhishek Verma", "Saurabh Khoria", "Disha"]
+  "Harshini":["Abhishek Verma", "Saurabh Khoria", "Disha"],
+  "Ashrith":["Saurabh Anand", "Vijith", "Shafeeq"]
 }
 var root = "Vedha";
 function removeDuplicates(heirarchyMap){
   let originalValues = new Set();
   for(let key in heirarchyMap) {
+    remainingNames.delete(key);
     let children = heirarchyMap[key];
     let nonDuplicateChildren = [];
     for(let childIndex in children){
+      console.log()
       if(!originalValues.has(children[childIndex])){
           nonDuplicateChildren.push(children[childIndex]);
+          originalValues.add(children[childIndex])
       }
+      remainingNames.delete(children[childIndex]);
     }
     heirarchyMap[key] = nonDuplicateChildren;
   }
 }
+
+
 removeDuplicates(map);
 
 
@@ -68,11 +114,21 @@ var dynamicTreeData = [
 ]
 
 function buildTree(root){
+  if(map[root.name] == null){
+    root.nodeSvgShape =  	{shape: 'circle', shapeProps: {r: 10, fill:'#FE7C00', strokeWidth: 0.5}}
+
+  }
+  else {
+  root.nodeSvgShape =  	{shape: 'circle', shapeProps: {r: 10, fill:'#FFFFB3', strokeWidth: 0.5}}
+  }
+
+  
    let children = map[root.name]
    root.children = [];
    for(let key in children){
      let child = children[key];
      let childNode = buildTree(buildTreeNode(child));
+     root.nodeSvgShape =  	{shape: 'circle', shapeProps: {r: 10, fill:'#00B4AB', strokeWidth: 0.5}};
      root.children.push(childNode);
    }
    return root;
@@ -93,32 +149,14 @@ class MyComponent extends React.Component {
   }
   
   render() {
-    let nodeStyle = {
-  stroke: 'blue',
-  strokeWidth: 3,
-};
-let leafNodeStyle = {
-  stroke: 'blue',
-  strokeWidth: 3,
-};
+    
+let textStyle = {};
     return (
+      
+      
       <div id="treeWrapper" style={{width: '100%', height: '50em',}} ref={tc => (this.treeContainer = tc)}>
     
-        <Tree data={dynamicTreeData} orientation="vertical" style={{
-  links: nodeStyle,
-  nodes: {
-    node: {
-      circle: nodeStyle,
-      name: nodeStyle,
-      attributes: nodeStyle,
-    },
-    leafNode: {
-      circle: leafNodeStyle,
-      name: leafNodeStyle,
-      attributes: leafNodeStyle,
-    },
-  }}
-}
+        <Tree data={dynamicTreeData} orientation="vertical" 
 translate={this.state.translate} 
 /> 
       </div>
@@ -127,7 +165,29 @@ translate={this.state.translate}
   }
 }
 
+class UnNominated extends React.Component {
+  render() {
+    let array = [];
+    console.log(remainingNames);
+    
+    remainingNames.forEach(x => array.push((<p class="floater">{x}</p>)));
+    
+    if(array.length) {
+    return (
+      
+      <div><p class="random">In case you are looking for folks who are not nominated yet, here you go</p><div>
+      {array}</div></div>
+      
+    );
+    }
+    return (<div><p>Yayy! We successfully covered everyone</p></div>);
+  }
+}
+
+
 /*
  * Render the above component into the div#app
  */
+
+render(<UnNominated />, document.getElementById('unnominated'));
 render(<MyComponent />, document.getElementById('root'));
